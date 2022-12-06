e946233745814923a87c5fbffd62c4089915a0e8
Klaas-Jan Stellema <klaasjan.stellema@sanoma.com>
2022-12-06 11:36:36 +0100

    wip

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,4 +1,21 @@
-fn main() {
-    let input = std::fs::read_to_string("src/input.txt").unwrap();
-    println!("{input}");
+use itertools::Itertools;
+
+fn main() -> color_eyre::Result<()> {
+    color_eyre::install()?;
+
+    let max = include_str!("input.txt")
+        .lines()
+        .map(|v| v.parse::<u64>().ok())
+        .batching(|it| {
+            let mut sum = None;
+            while let Some(Some(v)) = it.next() {
+                sum = Some(sum.unwrap_or(0) + v);
+            }
+            sum
+        })
+        .max();
+
+    println!("{max:?}");
+   
+    Ok(())
 }
