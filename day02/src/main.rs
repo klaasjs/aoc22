15cecff4af8ef159be79a3b7a0267dad29272de2
Klaas-Jan Stellema <klaasjan.stellema@sanoma.com>
2022-12-07 11:54:37 +0100

    day02

---
+++ day02/src/main.rs
@@ -0,0 +1,57 @@
+use std::str::FromStr;
+
+#[derive(Debug, Clone, Copy)]
+enum Move {
+    Rock,
+    Paper,
+    Scissors,
+}
+
+#[derive(Debug, Clone, Copy)]
+struct Round {
+    theirs: Move,
+    ours: Move
+}
+
+impl TryFrom<char> for Move {
+    type Error = color_eyre::Report;
+
+    fn try_from(c: char) -> Result<Self, Self::Error> {
+        match c {
+            'A' | 'X' => Ok(Move::Rock),
+            'B' | 'Y' => Ok(Move::Paper),
+            'C' | 'Z' => Ok(Move::Scissors),
+            _ => Err(color_eyre::eyre::eyre!("not a valid move {c:?}")),
+        }
+    }
+ }
+
+ impl FromStr for Round {
+    type Err = color_eyre::Report;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut chars = s.chars();
+        let (Some(theirs), Some(' '), Some(ours), None) = (chars.next(), chars.next(), chars.next(), chars.next()) else {
+            return Err(color_eyre::eyre::eyre!("expected <theirs>SP<ours>EOF, got {s:?}"));
+        };
+
+        Ok(Self {
+            theirs: theirs.try_into()?,
+            ours: ours.try_into()?,
+        })
+    }
+}
+
+fn main() -> color_eyre::Result<()> {
+    color_eyre::install();
+
+    for round in include_str!("input.txt")
+        .lines()
+        .map(|line| line.parse::<Round>())
+    {
+        let round = round?;
+        println!("{round:?}");
+    }
+
+    Ok(())
+}
