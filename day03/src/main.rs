022ab4c95928ed87e175db18977633785b947fc9
Klaas-Jan Stellema <klaasjan.stellema@sanoma.com>
2022-12-08 13:58:31 +0100

    bar

---
+++ day03/src/main.rs
@@ -0,0 +1,20 @@
+#[repr(transparent)]
+struct Item(u8);
+
+impl TryFrom<u8> for Item {
+    type Error = color_eyre::Report;
+
+    fn try_from(value: u8) -> Result<Self, Self::Error> {
+        match value {
+            b'a'..=b'z' | b'A'..=b'Z' => Ok(Item(value)),
+            _ => Err(color_eyre::eyre::eyre!("{} is not a valid item", value as char)),
+        }
+    }
+}
+
+fn main() -> color_eyre::Result<()> {
+    let _a = Item(b'a');
+    let _exclaim = Item(b'!');
+
+    Ok(())
+}
